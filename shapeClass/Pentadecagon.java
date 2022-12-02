ea25aa971bd466443adcb83e5e5a661f82ad54e1
Drew Miller <drew.miller@mrjaffesclass.com>
2022-12-02 11:44:44 -0800

    need-apothem

--- shapeClass/Pentadecagon.java
+++ shapeClass/Pentadecagon.java
@@ -1,17 +1,16 @@
 public class Pentadecagon implements Shape {
     private String color;
     private int sides;
-    private int distanceSideCenterToShapeCenter;
+    private int apothem;
     private int sideLength;
-    public Pentadecagon(int theDistanceSideCenterToShapeCenter, int theSideLength, String theColor) {
+    public Pentadecagon(int theSideLength, String theColor) {
         this.sides = 15;
         this.color = theColor;
-        this.distanceSideCenterToShapeCenter = theDistanceSideCenterToShapeCenter;
         this.sideLength = theSideLength;
     }
     
     public double getArea() {
-        return .5 * sideLength * distanceSideCenterToShapeCenter * 15;
+        return .5 * sideLength *  * sides;
     }
     
     public double getPerimeter() {
