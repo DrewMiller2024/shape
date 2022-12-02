bdb211d61fd930a1de828a0d46d4c77636381225
Drew Miller <drew.miller@mrjaffesclass.com>
2022-12-02 11:18:09 -0800

    shape-circle

---
+++ shapeClass/Shape.java
@@ -0,0 +1,22 @@
+public interface Shape {
+  /**
+   * Computes the area of the shape
+   * 
+   * @return    The computed area
+   */
+  double getArea();
+
+  /**
+   * Computes the perimeter of the shape
+   * 
+   * @return    The computed perimeter
+   */
+  double getPerimeter();
+
+  /**
+   * Gets the shape's color
+   * 
+   * @return    The color
+   */
+  String getColor();
+}
