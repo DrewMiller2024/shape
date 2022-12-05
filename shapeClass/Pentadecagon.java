6d0293ae5b3943c5c7a4242c0b10ee1e713afe8a
Drew Miller <drew.miller@mrjaffesclass.com>
2022-12-05 11:40:06 -0800

    shape-done

--- shapeClass/Pentadecagon.java
+++ shapeClass/Pentadecagon.java
@@ -1,16 +1,19 @@
 public class Pentadecagon implements Shape {
     private String color;
-    private int sides;
-    private int apothem;
-    private int sideLength;
+    private double sides;
+    private double apothem;
+    private double sideLength;
+    private double angle;
     public Pentadecagon(int theSideLength, String theColor) {
         this.sides = 15;
         this.color = theColor;
         this.sideLength = theSideLength;
+        this.angle = Math.toRadians(180 - (((360 / sides) / 2) + 90));
+        this.apothem = (sideLength / 2) * Math.tan(angle);
     }
     
     public double getArea() {
-        return .5 * sideLength *  * sides;
+        return 0.5 * sideLength * apothem * sides;
     }
     
     public double getPerimeter() {
